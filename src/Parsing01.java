ad7dd96d24f6fea28f1c5ff3e0c5023d50164acb
Aluno <Aluno@10.85.198.42>
2024-06-04 17:02:55 -0300

    java project Parsing01

---
+++ src/Parsing01.java
@@ -0,0 +1,23 @@
+public class Parsing01 {
+    public static void main(String[] args) {
+        //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
+        int shirtprice =15;
+        double TaxRate = 0.05;
+        double num3;
+        num3 = shirtprice * TaxRate;
+        
+        System.out.println(num3);
+        
+        
+        
+        //Parse shirtPrice and taxRate, and print the total tax
+        
+        
+        
+        //Try to parse taxRate as an int
+        
+        //Try to parse gibberish as an int
+        
+    }
+    
+}
